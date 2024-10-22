fd7809c68547b275810dc5e0edbf764c8bd8f56f
Angelo270204 <angelow.piscis.27@gmail.com>
2024-10-21 20:21:58 -0500

    Clase de la conexión añadido

--- src/main/java/util/DataSource.java
+++ src/main/java/util/DataSource.java
@@ -1,29 +1,30 @@
-
 package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DataSource {
-    private static final String URL = "jdbc:mysql://localhost:3306/sistema_inventario";
-  private static final String USUARIO = "root";
-  private static final String CONTRASENA = "123456";
-  private static Connection conexion = null;
-  /**
-   * Método para obtener la conexión a la base de datos.
-   * @return Objeto Connection
-   */
-  public static Connection obtenerConexion() {
-    if (conexion == null) {
-      try {
-        conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
-        System.out.println("Conexión establecida exitosamente.");
-      } catch (SQLException e) {
-        System.err.println("Error al conectar a la base de datos: " + e.getMessage());
-      }
+
+    private static final String URL = "jdbc:mysql://localhost:3306/inventario";
+    private static final String USUARIO = "root";
+    private static final String CONTRASENA = "1234";
+    private static Connection conexion = null;
+
+    /**
+     * Método para obtener la conexión a la base de datos.
+     *
+     * @return Objeto Connection
+     */
+    public static Connection obtenerConexion() {
+        if (conexion == null) {
+            try {
+                conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+                System.out.println("Conexión establecida exitosamente.");
+            } catch (SQLException e) {
+                System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            }
+        }
+        return conexion;
     }
-    return conexion;
-  }
 }
