fd031a2fdf76e1aeccc72378738b46f31efc09a1
MrPocoy0 <trysimp11@gmail.com>
2024-10-26 16:34:19 -0500

    Update cCategoria.java

--- src/main/java/controlador/cCategoria.java
+++ src/main/java/controlador/cCategoria.java
@@ -2,16 +2,22 @@ package controlador;
 
 import modelo.Categoria;
 import dao.CategoriaDao;
+import java.sql.Connection;
+import java.util.List;
 
 public class cCategoria {
     private final CategoriaDao categoriaDao;
 
-    public cCategoria() {
-        this.categoriaDao = new CategoriaDao();
+    public cCategoria(Connection connection) {
+        this.categoriaDao = new CategoriaDao(connection);
     }
 
     public boolean registrarCategoria(Categoria categoria) {
-        return categoriaDao.crear(categoria);
+        if (categoria == null) {
+            return false;
+        }
+        categoriaDao.addCategoria(categoria);
+        return true;
     }
 
     public Categoria obtenerCategoria(int id) {
@@ -19,10 +25,21 @@ public class cCategoria {
     }
 
     public boolean actualizarCategoria(Categoria categoria) {
+        if (categoria == null) {
+            return false;
+        }
         return categoriaDao.actualizar(categoria);
     }
 
     public boolean eliminarCategoria(int id) {
         return categoriaDao.eliminar(id);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Categoria> obtenerTodasLasCategorias() {
+        return categoriaDao.getAllCategorias();
     }
 }
